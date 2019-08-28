5c11291c5b6615c3a3d019e8db750cdfb2f43870
choiheesu <dqdq4197@naver.com>
2019-08-28 18:53:45 +0900

    weather_app end

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from './Weather';
+
+const API_KEY = "14dddb8db203786211fd2cc2bce648d5";
+
+export default class App extends Component{
+state = {
+  isLoaded : false,
+  error: null,
+  temperature:null,
+  name:null,
+};
+componentDidMount(){
+  navigator.geolocation.getCurrentPosition(
+    position =>{
+      return this._getWeather(position.coords.latitude, position.coords.longitude);
+    },
+    error =>{
+      this.setState({
+        error: error,
+      });
+    }
+  );
+}
+  _getWeather= (lat,long) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature:json.main.temp,
+        name: json.weather[0].main,
+        isLoaded:true,
+      })
+    });
+  }
+render() {
+  const { isLoaded,error,temperature,name } = this.state;
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={true}/>
+      {isLoaded ? (
+        <Weather 
+          weatherName={name}
+          temp={Math.floor(temperature - 273.15)}
+          /> 
+      ) : (
+         <View style={styles.loading}>
+           <Text style={styles.loadingText}>Getting the fucking weather</Text>
+           {error ? <Text style={styles.errorText}>{error}</Text> : null}
+         </View>
+        )}
+    </View>
+  );
+}
+}
+  const styles = StyleSheet.create({
+    container : {
+      flex :1,
+      backgroundColor: '#fff',
+    },
+    errorText:{
+      color:"red",
+      backgroundColor:"transparent",
+      marginBottom:50
+    },
+    loading : {
+      flex: 1,
+      backgroundColor:'#FDF6AA',
+      justifyContent: 'flex-end',
+      paddingLeft: 25,
+    },
+    loadingText: {
+      fontSize:38,
+      marginBottom: 100
+    }
+  });
